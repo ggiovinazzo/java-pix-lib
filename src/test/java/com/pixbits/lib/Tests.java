8f57e47d6b0144ecebc750df60dac1d7528cd1e0
Gianluca Giovinazzo <ggiovinazzo@mediadata.tv>
2020-02-20 18:15:57 +0100

    Fixes for test resources not readable when running tests from command line

--- src/test/java/com/pixbits/lib/Tests.java
+++ src/test/java/com/pixbits/lib/Tests.java
@@ -25,12 +25,12 @@ public class Tests
   public static String getResourceBase()
   {
     String basePackage = Tests.class.getPackage().getName().replaceAll("\\.", "/");
-    return basePackage + "/dummy/";
+    return "/dummy/";
   }
   
   public static Path getPathForReadableResource(String path)
   {
-    URL url = Tests.class.getClassLoader().getResource(Tests.getResourceBase()+path);
+    URL url = Tests.class.getResource(Tests.getResourceBase()+path);
     Path file = null;
     
     try
